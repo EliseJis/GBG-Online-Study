4729f93719ce861b310d132de080051958c611ed
Elise Oosterhuis <elise.j.oosterhuis@gmail.com>
2021-05-07 14:41:40 +0100

    Completed. Excluded participants, appended control tasks, and saved files.

--- TryVFdataextraction.R
+++ TryVFdataextraction.R
@@ -16,6 +16,11 @@ date = format(Sys.Date(), "%d%m%Y")
 path <- "D:/OneDrive - Lancaster University/PhD project/ParticipantTesting/Online Testing/Data/Raw data/All_data/VF/Answers/"
 path.out <- "D:/OneDrive - Lancaster University/PhD project/ParticipantTesting/Online Testing/Data/Raw data/All_data/Tidy Data/"
 
+stroop <- read.csv("./Tidy Data/Stroop_scores_27042021.csv")
+SoP <- read.csv("./Tidy Data/SoP_scores_27042021.csv")
+WM <- read.csv("./Tidy Data/WM_scores_27042021.csv")
+ToExclude <- read.csv("ToExclude2.csv") #Excluded participants
+
 ## Extract participant IDs
 
 FileList <- list.files(path, pattern= "*.xlsx", recursive=T)
@@ -23,19 +28,33 @@ ParticipantID <- str_extract(FileList, "\\d{7}")
 
 
 ## CATEGORY FLUENCY
+#Function data reads the first 6 rows of the Semantic VF of each file in the path folder.
 read_xlsx_files <- function(x){
   df_cat <- read_xlsx(path=paste(path, x, sep = "/"),sheet="Semantic",n_max = 6)
   return(df_cat)
 }
 
+#Binds the extracted rows together into one dataframe
 VF_cat <- lapply(FileList, read_xlsx_files) %>%
   bind_rows()
 
-VF_cat["ID"] <- rep(ParticipantID, each=6)
+#Adds participant ID
+VF_cat["ID"] <- as.numeric(rep(ParticipantID, each=6))
 
-VFcat_tidier <- VF_cat %>%
+#Tidy data
+VFcat_tidiest <- VF_cat %>%
   select(ID, Total, Measures, Animals, Vehicles, `Fruits and Vegetables`, Fluid, `Writing Utensils`) %>%
-  mutate_if(is.numeric, round)
+  mutate_if(is.numeric, round)  %>% 
+  #Remove incomplete participants and excluded participants
+  filter(!(ID %in% ToExclude)) %>%
+  left_join(stroop, by=c("ID" = "Participant.Private.ID")) %>%
+  left_join(SoP, by=c("ID"="Participant.Private.ID")) %>%
+  left_join(WM, by=c("ID"="Participant.Private.ID"))
+
+
+#Save data
+rio::export(VFcat_tidiest, paste0(path.out, date, "_VFcat_tidiest.csv"))
+
 
 ## ACTION FLUENCY
 read_xlsx_files <- function(x){
@@ -46,11 +65,19 @@ read_xlsx_files <- function(x){
 VF_act <- lapply(FileList, read_xlsx_files) %>%
   bind_rows()
 
-VF_act["ID"] <- rep(ParticipantID, each=6)
+VF_act["ID"] <- as.numeric(rep(ParticipantID, each=6))
 
-VFact_tidier <- VF_act %>%
+VFact_tidiest <- VF_act %>%
   select(ID, Total, Measures, `Things people do`, `Egg`) %>%
-  mutate_if(is.numeric, round)
+  mutate_if(is.numeric, round) %>% 
+  #Remove incomplete participants and excluded participants
+  filter(!(ID %in% ToExclude)) %>%
+  left_join(stroop, by=c("ID" = "Participant.Private.ID")) %>%
+  left_join(SoP, by=c("ID"="Participant.Private.ID")) %>%
+  left_join(WM, by=c("ID"="Participant.Private.ID"))
+
+rio::export(VFact_tidiest, paste0(path.out, date, "_VFact_tidiest.csv"))
+
 
 ## Letter FLUENCY
 read_xlsx_files <- function(x){
@@ -61,11 +88,16 @@ read_xlsx_files <- function(x){
 VF_let <- lapply(FileList, read_xlsx_files) %>%
   bind_rows()
 
-VF_let["ID"] <- rep(ParticipantID, each=6)
+VF_let["ID"] <- as.numeric(rep(ParticipantID, each=6))
 
-VFlet_tidier <- VF_let %>%
+VFlet_tidiest <- VF_let %>%
   select(ID, Total, Measures, M, P, S) %>%
   mutate_if(is.numeric, round) %>%
-  pivot_longer(!ID, names_to = "income", values_to = "count")
+  #Remove incomplete participants and excluded participants
+  filter(!(ID %in% ToExclude)) %>%
+  left_join(stroop, by=c("ID" = "Participant.Private.ID")) %>%
+  left_join(SoP, by=c("ID"="Participant.Private.ID")) %>%
+  left_join(WM, by=c("ID"="Participant.Private.ID"))
 
+rio::export(VFlet_tidiest, paste0(path.out, date, "_VFlet_tidiest.csv"))
 
